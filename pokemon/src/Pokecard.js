c3f32e1401fa98c821da86125190f1f7e120ef67
danieljanderson <danieljanderson0616@gmail.com>
2020-10-21 19:29:55 -0400

    done with pokemon game

--- pokemon/src/Pokecard.js
+++ pokemon/src/Pokecard.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react'
 import './Pokecard.css'
 const POKE_API = 
-'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+//'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+let padToThree =(number) =>(number<=999 ? `000${number}`.slice(-3):number)
 class Pokecard extends Component{
 render(){
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return(
        
         <div className="Pokecard">
-            <h1>{this.props.name}</h1>
+            <h1 className="Pokecard-title">{this.props.name}</h1>
             <img src ={imgSrc}></img>
-            <div>Type:{this.props.type}</div>
-            <div>EXP:{this.props.exp}</div>
+            <div className="Pokecard-data">Type:{this.props.type}</div>
+            <div className="Pokecard-data">EXP:{this.props.exp}</div>
         </div>
     )
 }
