664dd2c1345ac0100ff92dc186d9bb192aabd557
danieljanderson <danieljanderson0616@gmail.com>
2021-08-07 15:30:55 -0400

    finished with the project

--- lightsout/src/Board.js
+++ lightsout/src/Board.js
@@ -64,6 +64,7 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    
     let {ncols, nrows} = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -76,25 +77,34 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
+    flipCell(y,x)
+    flipCell(y,x-1)
+    flipCell(y,x+1)
+    flipCell(y-1,x)
+    flipCell(y+1,x)
 
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-
-   // this.setState({board, hasWon});
+    let hasWon = board.every(row=>row.every(cell=> !cell))
+    console.log(hasWon)
+    this.setState({board, hasWon});
   }
 
 
   /** Render game board or winning message. */
 
   render() {
+    if(this.state.hasWon){
+      return <hi>You Won!!!</hi>
+    }
     let tblBoard = []
     for(let y = 0; y<this.props.nrows;y++){
       let row = []
       for (let x=0; x<this.props.ncols;x++){
         let coord=`${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>) 
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={()=>this.flipCellsAround(coord)}/>) 
       }
       tblBoard.push(<tr key={y}>{row}</tr>)
     }
